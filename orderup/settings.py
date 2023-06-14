8d80162cc196f207ac9641a8c068b507a193d11d
jjpham <phamjason96@gmail.com>
2023-06-14 13:39:30 -0700

    Deployment bug pt.2

--- orderup/settings.py
+++ orderup/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'orderup.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': dj_database_url.config(     
-#     default='postgresql://postgres:postgres@localhost:5432/pham',        
-#     conn_max_age=600    )
-# }
+DATABASES = {
+    'default': dj_database_url.config(     
+    default='postgresql://postgres:postgres@localhost:5432/pham',        
+    conn_max_age=600    )
+}
 
 DATABASES = {
   'default': {
